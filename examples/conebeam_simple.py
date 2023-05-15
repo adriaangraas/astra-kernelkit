b597c624b32adbc98ace6d2279e75c38a4aa41d4
Adriaan Graas <>
2023-05-15 10:59:43 +0200

    Update examples

--- examples/conebeam_simple.py
+++ examples/conebeam_simple.py
@@ -12,12 +12,12 @@ geom_t0 = Geometry(
     Detector(99, 141, .01, .01))
 geom_t0 = rotate(geom_t0, yaw=.5*np.pi, roll=.2 * np.pi, pitch=.1 * np.pi)
 
-angles = np.linspace(0, 2 * np.pi, 719)
+angles = np.linspace(0, 2 * np.pi, 719, False)
 geoms = [rotate(geom_t0, yaw=a) for a in angles]
-vol_min, vol_max = [-.2] * 3, [.2] * 3
+vol_min, vol_max = [-.2, -.2, -.2 * 1.2], [.2, .2, .2 * 1.2]
 
 # cube with random voxels
-vol = np.zeros([100] * 3)
+vol = np.zeros([100, 100, 120])
 vol[25:75, 25:75, 25:75] = np.random.random([50] * 3)
 vol[35:65, 35:65, 35:65] += np.random.random([30] * 3)
 vol[45:55, 45:55, 45:55] += 1.
@@ -28,14 +28,14 @@ plt.figure()
 for p in projs[0:100:2]:
     plt.cla()
     plt.imshow(p)
-    plt.pause(.1)
+    plt.pause(.001)
 plt.close()
 
 # backproject
 vol2 = fdk(projs, geoms, vol.shape, vol_min, vol_max)
 plt.figure()
-for sl in range(0, vol2.shape[-1]):
+for sl in range(0, vol2.shape[-2], 4):
     plt.cla()
     plt.imshow(vol2[..., sl], vmin=0, vmax=.02)
-    plt.pause(.1)
+    plt.pause(.002)
 plt.close()
--- examples/conebeam_sirt.py
+++ examples/conebeam_sirt.py
@@ -12,12 +12,12 @@ geom_t0 = Geometry(
     Detector(99, 141, .01, .01))
 
 geom_t0 = rotate(geom_t0, yaw=.5*np.pi, roll=.2 * np.pi, pitch=.1 * np.pi)
-angles = np.linspace(0, 2 * np.pi, 1209)
-geoms = [rotate(geom_t0, yaw=a, pitch=a, roll=a) for a in angles]
+angles = np.linspace(0, 2 * np.pi, 200, False)
+geoms = [rotate(geom_t0, yaw=a) for a in angles]
 vol_min, vol_max = [-.2] * 3, [.2] * 3
 
 # cube with random voxels
-vol = np.zeros([300] * 3)
+vol = np.zeros([100] * 3)
 vol[25:75, 25:75, 25:75] = np.random.random([50] * 3)
 vol[35:65, 35:65, 35:65] += np.random.random([30] * 3)
 vol[45:55, 45:55, 45:55] += 1.
@@ -25,19 +25,17 @@ vol[45:55, 45:55, 45:55] += 1.
 # forward project
 projs = fp(vol, geoms, vol_min, vol_max)
 plt.figure()
-for p in projs[0:100:2]:
+for p in projs[0:100:4]:
     plt.cla()
     plt.imshow(p)
-    plt.pause(.1)
+    plt.pause(.001)
 plt.close()
 
-vol2 = sirt_experimental(projs, geoms, vol.shape, vol_min, vol_max)
-import pyqtgraph as pq
-pq.image(vol2)
+vol2 = sirt_experimental(projs, geoms, vol.shape, vol_min, vol_max,
+                         iters=200)
 plt.figure()
-plt.show()
 for sl in range(25, vol2.shape[-1]):
     plt.cla()
     plt.imshow(vol2[..., sl], vmin=0, vmax=2.)
-    plt.pause(.2)
+    plt.pause(.001)
 plt.close()
