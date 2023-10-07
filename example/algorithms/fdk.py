95c833a8f1324f4b43879efcd4de3f319274ea31
Adriaan Graas <>
2023-10-07 15:58:12 +0200

    Write without `__main__`

--- example/algorithms/fdk.py
+++ example/algorithms/fdk.py
@@ -1,47 +1,43 @@
-"""Demonstrates a Feldmann-Davis-Kress algorithm."""
+"""Demonstrates a Feldkamp-Davis-Kress algorithm."""
 import kernelkit as kk
 import matplotlib.pyplot as plt
 import numpy as np
 
+# projection geometries of a cube in the 3D space
+geom_t0 = kk.ProjectionGeometry(
+    source_position=[-10.0, 0.0, 0.0],
+    detector_position=[20.0, 0.0, 0.0],
+    detector=kk.Detector(rows=150, cols=200, pixel_height=0.01, pixel_width=0.01),
+)
+angles = np.linspace(0, 2 * np.pi, 1000, False)
+proj_geoms = [kk.rotate(geom_t0, yaw=a) for a in angles]
 
-def geometries(n_angles=1000):
-    """Projection geometries of a cube in the 3D space."""
-    geom_t0 = kk.ProjectionGeometry(
-        source_position=[-10.0, 0.0, 0.0],
-        detector_position=[20.0, 0.0, 0.0],
-        detector=kk.Detector(rows=150, cols=200, pixel_height=0.01, pixel_width=0.01),
-    )
-    angles = np.linspace(0, 2 * np.pi, n_angles, False)
-    return [kk.rotate(geom_t0, yaw=a) for a in angles]
+# volume and volume geometries
+vol = np.zeros((100, 110, 120))  # a cube with random voxels
+vol[25:45, 25:45, 25:45] = np.random.random([20] * 3)
+vol[35:65, 35:65, 35:65] += np.random.random([30] * 3)
+vol_geom = kk.resolve_volume_geometry(
+    vol.shape, [-0.2, None, None], [0.2, None, None], verbose=False
+)
 
+# generate projections by forward projection
+projs = kk.fp(vol, proj_geoms, vol_geom)
 
-if __name__ == "__main__":
-    proj_geoms = geometries()
-    vol = np.zeros((100, 110, 120))  # a cube with random voxels
-    vol[25:45, 25:45, 25:45] = np.random.random([20] * 3)
-    vol[35:65, 35:65, 35:65] += np.random.random([30] * 3)
-    vol_geom = kk.resolve_volume_geometry(
-        vol.shape, [-0.2, None, None], [0.2, None, None], verbose=False
-    )
+plt.figure()
+for p in projs[::16]:
+    plt.cla()
+    plt.title("Forward projection")
+    plt.imshow(p)
+    plt.pause(0.001)
+plt.close()
 
-    # forward project
-    projs = kk.fp(vol, proj_geoms, vol_geom)
+# backproject with the Feldkamp-Davis-Kress algorithm
+vol2 = kk.fdk(projs, proj_geoms, vol_geom)
 
-    plt.figure()
-    for p in projs[::16]:
-        plt.cla()
-        plt.title("Forward projection")
-        plt.imshow(p)
-        plt.pause(0.001)
-    plt.close()
-
-    # backproject
-    vol2 = kk.fdk(projs, proj_geoms, vol_geom)
-
-    plt.figure()
-    for sl in range(0, vol2.shape[-2], 2):
-        plt.title("Feldkamp-Davis-Kress")
-        plt.cla()
-        plt.imshow(vol2[..., sl], vmin=0, vmax=0.05)
-        plt.pause(0.001)
-    plt.close()
+plt.figure()
+for sl in range(0, vol2.shape[-2], 2):
+    plt.title("Feldkamp-Davis-Kress")
+    plt.cla()
+    plt.imshow(vol2[..., sl], vmin=0, vmax=0.05)
+    plt.pause(0.001)
+plt.close()
--- example/algorithms/sirt.py
+++ example/algorithms/sirt.py
@@ -1,45 +1,46 @@
+"""Demonstrates a SIRT algorithm with callback function."""
 import kernelkit as kk
 import matplotlib.pyplot as plt
 import numpy as np
 
 
-def geometries(n_angles=250):
-    """Projection geometries of a cube in the 3D space."""
-    geom_t0 = kk.ProjectionGeometry(
-        source_position=[-10.0, 0.0, 0.0],
-        detector_position=[20.0, 0.0, 0.0],
-        detector=kk.Detector(rows=150, cols=200, pixel_height=0.01, pixel_width=0.01),
-    )
-    angles = np.linspace(0, 2 * np.pi, n_angles, False)
-    return [kk.rotate(geom_t0, yaw=a) for a in angles]
+# Projection geometries of a cube in the 3D space
+geom_t0 = kk.ProjectionGeometry(
+    source_position=[-10.0, 0.0, 0.0],
+    detector_position=[20.0, 0.0, 0.0],
+    detector=kk.Detector(rows=150, cols=200, pixel_height=0.01, pixel_width=0.01),
+)
+angles = np.linspace(0, 2 * np.pi, 250, False)
+proj_geoms = [kk.rotate(geom_t0, yaw=a) for a in angles]
+
+# volume and volume geometry
+gt = np.zeros((100, 100, 100))
+gt[25:75, 25:75, 25:75] = np.random.random([50] * 3)
+gt[35:65, 35:65, 35:65] += np.random.random([30] * 3)
+gt[45:55, 45:55, 45:55] += 1.0
+vol_geom = kk.resolve_volume_geometry(
+    shape=gt.shape, extent_min=[-0.2] * 3, extent_max=[0.2] * 3
+)
+
+# simulate some projections
+projs = kk.fp(gt, proj_geoms, vol_geom)
+
+# set up a plotting callback function
+fig, axs = plt.subplots(1, 2)
 
 
-if __name__ == "__main__":
-    proj_geoms = geometries()
-    vol_geom = kk.resolve_volume_geometry(
-        shape=[100] * 3, extent_min=[-0.2] * 3, extent_max=[0.2] * 3
-    )
+def callback(i, x, y):
+    """Callback function that plots residual projections and the volume."""
+    j = (i * 10) % len(proj_geoms)
+    axs[0].clear()
+    axs[0].set_title(f"Iter {i}: residual projection {j}")
+    axs[0].imshow(y[j].get(), vmin=-0.25, vmax=0.25)
+    axs[1].clear()
+    k = 25 + ((i * 5) % 50)
+    axs[1].set_title(f"Iter {i}: volume slice {k}")
+    axs[1].imshow(x[..., k].get(), vmax=3.0)
+    plt.pause(00.01)
 
-    # simulate some projections
-    gt = np.zeros(vol_geom.shape)
-    gt[25:75, 25:75, 25:75] = np.random.random([50] * 3)
-    gt[35:65, 35:65, 35:65] += np.random.random([30] * 3)
-    gt[45:55, 45:55, 45:55] += 1.0
-    projs = kk.fp(gt, proj_geoms, vol_geom)
 
-    fig, axs = plt.subplots(1, 2)
-    def callback(i, x, y):
-        """Callback function that plots residual projections and the volume."""
-        j = (i * 10) % len(proj_geoms)
-        axs[0].clear()
-        axs[0].set_title(f"Iter {i}: residual projection {j}")
-        axs[0].imshow(y[j].get(), vmin=-0.25, vmax=0.25)
-        axs[1].clear()
-        k = 25 + ((i * 5) % 50)
-        axs[1].set_title(f"Iter {i}: volume slice {k}")
-        axs[1].imshow(x[..., k].get(), vmax=3.0)
-        plt.pause(00.01)
-
-    # run the Simultaneous Iterative Reconstruction Technique (SIRT)
-    _ = kk.sirt(projs, proj_geoms, vol_geom, iters=2000, callback=callback)
-    plt.show()
+# run the Simultaneous Iterative Reconstruction Technique (SIRT)
+kk.sirt(projs, proj_geoms, vol_geom, iters=200, callback=callback)
