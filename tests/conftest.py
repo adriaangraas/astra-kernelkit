77d52733bee3610c5930c71d071deb025c9aa2ff
Adriaan Graas <>
2023-10-09 15:20:55 +0200

    Fix test imports

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,6 @@ import cupy as cp
 import numpy as np
 import pytest
 
-from astrapy.geom.proj import ProjectionGeometry, Detector
-from astrapy.kernel import copy_to_texture
+from kernelkit.geom.proj import ProjectionGeometry, Detector
+from kernelkit.kernel import copy_to_texture
 
